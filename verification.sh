5f3aec63ebc6bf1cd36d6c169854cdc116d6706f
ttu <tomi.tuhkanen@iki.fi>
2023-02-18 18:40:04 +0200

    update verification script

--- verification.sh
+++ verification.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-rm -rf venv_py
+rm -rf .venv_py
 
 # If python command is for 2.7, replace next line with: python3 -m venv venv_py
-python -m venv venv_py
-source venv_py/bin/activate
+python -m venv .venv_py
+source .venv_py/bin/activate
+
+
 if [ "$1" = "pypi" ]; then
   python -m pip install ruuvitag_sensor
+elif [ "$1" = "testpypi" ]; then
+  python -m pip install -i https://test.pypi.org/simple/ ruuvitag-sensor
 else
+  python -m pip install --upgrade pip
   python -m pip install -e .
 fi
 
 if [ "$RUUVI_BLE_ADAPTER" = "Bleson" ]; then
   python -m pip install git+https://github.com/TheCellule/python-bleson
+elif [ "$RUUVI_BLE_ADAPTER" = "Bleak" ]; then
+  python -m pip install bleak
 fi
 
 python verification.py
